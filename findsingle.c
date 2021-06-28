3c04118c708063e9ce8105366133504c4e832aa9
Prasanth Ganesan <vvmsanth56@gmail.com>
2021-06-28 13:16:35 +0530

    no need count variable

--- findsingle.c
+++ findsingle.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 #define N 9
 int main(){
-    int couple[N]={1, 4, 7, 3, 2, 1, 4, 3,2};
+    int couple[N]={1, 4, 2, 3, 2, 1, 4, 3,7};
     int pos[N];
-    int count=0;
      for (int i = 0; i < N; i++) {
   for (int j = i+1; j < N; j++) {
     if(couple[i] == couple[j]){
          pos[i]=i;
          pos[j]=j;
-         count=count +2;
     }
   }
 }
-for(int k=0;k<count;k++){
+for(int k=0;k<N;k++){
     if(pos[k] != k)
     {
         printf("%d",couple[k]);
