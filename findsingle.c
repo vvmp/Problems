23440679dc560f978433984a2cfba972100f4631
Prasanth Ganesan <vvmsanth56@gmail.com>
2021-06-28 13:08:19 +0530

    equal value postion are storing in another array

--- findsingle.c
+++ findsingle.c
@@ -3,5 +3,14 @@ int main(){
     int couple[N]={1, 4, 7, 3, 2, 1, 4, 3,2};
     int pos[N];
     int count=0;
+     for (int i = 0; i < N; i++) {
+  for (int j = i+1; j < N; j++) {
+    if(couple[i] == couple[j]){
+         pos[i]=i;
+         pos[j]=j;
+         count=count +2;
+    }
+  }
+}
     return 0;
 }
