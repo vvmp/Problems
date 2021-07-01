e944219afbb29c31abc1ea862dc78988930f2be9
Prasanth Ganesan <vvmsanth56@gmail.com>
2021-07-01 22:46:34 +0530

    adding new file

--- findsingle.c
+++ findsingle.c
@@ -1,21 +1,34 @@
-#include<stdio.h>
-#define N 9
-int main(){
-    int couple[N]={1, 4, 2, 3, 2, 1, 4, 3,7};
-    int pos[N];
-     for (int i = 0; i < N; i++) {
-  for (int j = i+1; j < N; j++) {
-    if(couple[i] == couple[j]){
-         pos[i]=i;
-         pos[j]=j;
+#include <stdio.h>
+#include<stdbool.h>
+int findsingle(int n,int couple[]){
+        int single;
+     for(int i = 0; i < n; i++) 
+     {
+         if(couple[i] != -1){
+            bool targetisfound=false;
+        for(int j = i+1; j < n; j++) 
+        {
+            if(couple[i] == couple[j])
+            {
+                couple[i]=-1;
+                couple[j]=-1;
+                targetisfound=true;
+                break;
+            }
+        }
+          if(targetisfound == false){
+      single=couple[i];
+  } 
     }
-  }
+
 }
-for(int k=0;k<N;k++){
-    if(pos[k] != k)
-    {
-        printf("%d",couple[k]);
-    }
+return single;
 }
+int main()
+{
+    int couple[]={1,1,2,2,3,3,4,4,5,5,6,6,7,7,8};
+    int size = sizeof(couple) / sizeof(int);
+    int single=findsingle(size,couple);
+    printf("%d",single);
     return 0;
 }
